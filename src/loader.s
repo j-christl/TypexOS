07ecbd549b2c1dd0641349f8a9681e437f52ed57
inplex <christl.joshua@googlemail.com>
2014-03-09 15:47:32 +0100

    Create loader.s

---
+++ src/loader.s
@@ -0,0 +1,38 @@
+global loader
+global magic
+global mbd
+
+extern kmain
+extern println
+
+MODULEALIGN equ  1<<0
+MEMINFO     equ  1<<1
+FLAGS       equ  MODULEALIGN | MEMINFO
+MAGIC       equ  0x1BADB002
+CHECKSUM    equ -(MAGIC + FLAGS)
+
+section .text
+
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+STACKSIZE equ 0x4000
+
+loader:
+    mov  esp, stack + STACKSIZE
+    mov  [magic], eax
+    mov  [mbd], ebx
+    call kmain
+    cli
+.hang:
+    hlt
+    jmp  .hang
+
+section .bss
+
+align 4
+stack: resb STACKSIZE
+magic: resd 1
+mbd:   resd 1
